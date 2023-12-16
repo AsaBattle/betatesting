91c512a15bcbd57a468adc34b590251c20a99b5f
Asa a <asanbattle@gmail.com>
2023-12-15 17:58:47 -0800

    Added auth.js

---
+++ pages/auth.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Auth() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Get the user object from the query parameters
+    const user = router.query.user;
+    const code = router.query.code;
+
+    if (user && code) {
+      // Save the user object and the code in the local storage
+      localStorage.setItem('user', user);
+      localStorage.setItem('code', code);
+
+      // Redirect the user to the dashboard page
+      router.push('/paint');
+    }
+  }, [router]);
+
+  return null;
+}
