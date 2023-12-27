75a0199936725b5850768632f0743a236915169a
Asa a <asanbattle@gmail.com>
2023-12-27 15:43:12 -0800

    First commit, Added login changes

---
+++ pages/auth.js
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Auth({ isAuthenticated }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    console.log("Inside useEffect in Auth.js isAuthenticated: " + isAuthenticated);
+    
+    // Redirect based on the authentication status
+    if (isAuthenticated) {
+      router.push('/paint');
+    } else {
+      router.push('/login');
+    }
+  }, [router, isAuthenticated]);
+
+  return null;
+}
+
+export async function getServerSideProps(context) {
+  /*
+  const { req } = context;
+  const userSessionCookie = req.cookies['discord.oauth2']; // Replace with your actual session cookie name
+
+  console.log("Inside getServerSideProps in Auth.js userSessionCookie: " + userSessionCookie);
+  if (userSessionCookie) {
+    // If there is a session cookie, consider the user as authenticated
+    return {
+      props: {
+        isAuthenticated: true,
+      },
+    };
+  }
+*/
+  // If there is no session cookie, consider the user as not authenticated
+  return {
+    props: {
+      isAuthenticated: true,
+    },
+  };
+}
--- pages/paint.js
+++ pages/paint.js
@@ -1,22 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 import Head from "next/head";
-import Link from "next/link";
 import Canvas from "components/canvas";
 import PromptForm from "components/prompt-form";
 import Dropzone from "components/dropzone";
 import Download from "components/download";
+import axios from "axios";
 import { XCircle as StartOverIcon } from "lucide-react";
-import { Code as CodeIcon } from "lucide-react";
-import { Rocket as RocketIcon } from "lucide-react";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export default function Home() {
+export default function Home( theUserData ) {
   const [predictions, setPredictions] = useState([]);
   const [error, setError] = useState(null);
   const [maskImage, setMaskImage] = useState(null);
   const [userUploadedImage, setUserUploadedImage] = useState(null);
   const [brushSize, setBrushSize] = useState(40); // Default brush size
+  const [userData, setUserData] = useState(null);
+  
+  const router = useRouter();
+
+    // Add a logout function
+    const handleLogout = () => {
+      // Redirect to the logout URL
+      window.location.href = 'https://www.fulljourney.ai/api/auth/logoutnextjs';
+    };
+
+  useEffect(() => {
+    // Check user login status on component mount
+    checkUserLogin();
+  }, []);
+
+  const checkUserLogin = async () => {
+    /*
+    const response = await axios.get("https://www.fulljourney.ai/api/auth/set-test", { withCredentials: true }); 
+    const response2 = await axios.get("https://www.fulljourney.ai/api/auth/get-test", { withCredentials: true }); 
+    
+    console.log("the response is: ",response);
+    console.log("the response is: ",response.data);
+    console.log("the response2 is: ",response2);
+    console.log("the response2 is: ",response2.data);
+    //const response = await axios.get("https://www.fulljourney.ai/api/auth/", { withCredentials: true });
+      //console.log("the response is: ",response);
+      //console.log("the response is: ",response.data);
+
+      //setUser(response.data);
+      for (var i=1;i<30;++i)
+       console.log("11111111111111111111111111111111");
+      */
+    //  console.log('User authenticated', response.data);
+    if (theUserData)       
+    {
+      console.log("theUserData is: ",theUserData);
+      if (theUserData.userData)
+      {
+        console.log("theUserData.userData is: ",theUserData.userData);
+        setUserData(theUserData.userData);
+      }
+      else
+      {
+        console.log("theUserData.userData is null");
+      }
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,10 +76,7 @@ export default function Home() {
       prompt: e.target.prompt.value,
       image: userUploadedImage
         ? await readAsDataURL(userUploadedImage)
-        : // only use previous prediction as init image if there's a mask
-        maskImage
-        ? prevPredictionOutput
-        : null,
+        : maskImage ? prevPredictionOutput : null,
       mask: maskImage,
     };
 
@@ -44,35 +87,33 @@ export default function Home() {
       },
       body: JSON.stringify(body),
     });
+
     const prediction = await response.json();
 
     if (response.status !== 201) {
       setError(prediction.detail);
       return;
     }
+
     setPredictions(predictions.concat([prediction]));
 
-    while (
-      prediction.status !== "succeeded" &&
-      prediction.status !== "failed"
-    ) {
+    while (prediction.status !== "succeeded" && prediction.status !== "failed") {
       await sleep(1000);
       const response = await fetch("/api/predictions/" + prediction.id);
-      prediction = await response.json();
+      const updatedPrediction = await response.json();
       if (response.status !== 200) {
-        setError(prediction.detail);
+        setError(updatedPrediction.detail);
         return;
       }
-      setPredictions(predictions.concat([prediction]));
+      setPredictions(currentPredictions => currentPredictions.concat([updatedPrediction]));
 
-      if (prediction.status === "succeeded") {
+      if (updatedPrediction.status === "succeeded") {
         setUserUploadedImage(null);
       }
     }
   };
 
-  const startOver = async (e) => {
-    e.preventDefault();
+  const startOver = () => {
     setPredictions([]);
     setError(null);
     setMaskImage(null);
@@ -86,28 +127,28 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <p className="pb-5 text-xl text-white text-center font-helvetica">
-        <strong>FullJourney.AI Inpainting</strong>
+        <strong>FullJourney.AI Inpainting Greatness</strong>
       </p>
       <p className="pb-2 text-xl text-gray-500 text-center font-helvetica">
-  <strong>Draw over the areas you want replaced...</strong>
-</p>
-<main className="container mx-auto p-2">
-  {error && <div>{error}</div>}
+        <strong>Draw over the areas you want replaced...</strong>
+      </p>
+      <main className="container mx-auto p-2">
+        {error && <div>{error}</div>}
 
-  {/* Brush size slider */}
-  <div className="brush-slider-container text-white flex items-center justify-center mx-auto" style={{ width: '30%' }}>
-  <label htmlFor="brushSize" className="flex-shrink-0 mr-2">Brush Size: {brushSize}</label>
-  <input
-    type="range"
-    id="brushSize"
-    name="brushSize"
-    min="1"
-    max="100"
-    value={brushSize}
-    onChange={(e) => setBrushSize(Number(e.target.value))}
-    className="brush-slider flex-grow"
-  />
-</div>
+        {/* Brush size slider */}
+        <div className="brush-slider-container text-white flex items-center justify-center mx-auto" style={{ width: '30%' }}>
+        <label htmlFor="brushSize" className="flex-shrink-0 mr-2">Brush Size: {brushSize}</label>
+        <input
+          type="range"
+          id="brushSize"
+          name="brushSize"
+          min="1"
+          max="100"
+          value={brushSize}
+          onChange={(e) => setBrushSize(Number(e.target.value))}
+          className="brush-slider flex-grow"
+        />
+      </div>
 
         <div className="border-hairline max-w-[512px] mx-auto relative">
           <Dropzone
@@ -146,6 +187,15 @@ export default function Home() {
           </div>
         </div>
       </main>
+       {/* Add a logout button at the bottom of the page */}
+       <footer className="text-center my-4">
+        <button 
+          onClick={handleLogout} 
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
+      </footer>
     </div>
   );
 }
@@ -160,3 +210,37 @@ function readAsDataURL(file) {
     fr.readAsDataURL(file);
   });
 }
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const cookies = req.headers.cookie || '';
+
+  // Try grabbing the users data. IF we can get it, it means they've logged in successfully
+  try {
+    const response = await axios.get('https://www.fulljourney.ai/api/auth/', {
+      headers: { Cookie: cookies },
+      withCredentials: true,
+    });
+
+    // Assuming the response contains the user data you need
+    const userData = response.data;
+
+    // Return the user data as props
+    return { props: { userData } };
+  } catch (error) {
+    console.error('Error:', error);
+
+    // If there's an error, you can redirect or return empty props
+    return {
+      redirect: {
+        destination: '/login',  // example redirect
+        permanent: false,
+      },
+    };
+
+    // Or just return empty props
+    // return { props: {} };
+  }
+}
+
+
