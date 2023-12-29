6faf23d5d1f31bc7dee334d044aacd554c842d7c
Asa a <asanbattle@gmail.com>
2023-12-28 17:32:18 -0800

    fixed login and succeed message

--- pages/paint.js
+++ pages/paint.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 import Head from "next/head";
 import Canvas from "components/canvas";
 import PromptForm from "components/prompt-form";
@@ -9,86 +10,117 @@ import { XCircle as StartOverIcon } from "lucide-react";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export default function Home({ userData }) {
+export default function Home(theUserData) {
   const [predictions, setPredictions] = useState([]);
   const [error, setError] = useState(null);
   const [maskImage, setMaskImage] = useState(null);
   const [userUploadedImage, setUserUploadedImage] = useState(null);
   const [brushSize, setBrushSize] = useState(40); // Default brush size
+  const [userData, setUserData] = useState(null);
 
-  useEffect(() => {
-    console.log("Checking User Login");
-    // Check user login status on component mount
-    if (userData) {
-      console.log("User data:", userData);
-    } else {
-      console.log("No user data received");
-    }
-  }, [userData]);
+  const router = useRouter();
 
+  // Add a logout function
   const handleLogout = () => {
     window.location.href = 'https://www.fulljourney.ai/api/auth/logoutnextjs';
   };
+
+  useEffect(() => {
+    checkUserLogin();
+  }, []);
+
+  const checkUserLogin = async () => {
+    if (theUserData) {
+      console.log("theUserData is: ", theUserData);
+      if (theUserData.userData) {
+        console.log("theUserData.userData is: ", theUserData.userData);
+        setUserData(theUserData.userData);
+      } else {
+        console.log("theUserData.userData is null");
+      }
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const promptValue = e.target.prompt.value;
   
-    try {
-      // Get the last successful prediction's output, if it exists
-      const lastOutput = predictions.length > 0 && predictions[predictions.length - 1].status === 'succeeded'
-        ? predictions[predictions.length - 1].output
-        : null;
+    const prevPrediction = predictions[predictions.length - 1];
+    const prevPredictionOutput = prevPrediction?.output
+      ? prevPrediction.output[prevPrediction.output.length - 1]
+      : null;
   
-      // Determine the image to use: if there's a mask, use the last successful output; otherwise, use the user uploaded image
-      const imageToUse = maskImage && lastOutput ? lastOutput : userUploadedImage;
+    const body = {
+      prompt: e.target.prompt.value,
+      image: userUploadedImage
+        ? await readAsDataURL(userUploadedImage)
+        : maskImage ? prevPredictionOutput : null,
+      mask: maskImage,
+    };
   
-      // If there's no image to use, don't proceed
-      if (!imageToUse) {
-        setError('No image to process.');
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+  
+    const prediction = await response.json();
+  
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      console.error("Error in prediction response: ", prediction.detail); // Added logging for error detail
+      return;
+    }
+  
+    setPredictions(predictions.concat([prediction]));
+  
+    // Added Debugging Logs
+    console.log("Initial prediction status:", prediction.status);
+  
+    while (prediction.status !== "succeeded" && prediction.status !== "failed") {
+      await sleep(1000);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      const updatedPrediction = await response.json();
+      if (response.status !== 200) {
+        setError(updatedPrediction.detail);
+        console.error("Error in updating prediction: ", updatedPrediction.detail); // Added logging for error detail
         return;
       }
   
-      // If userUploadedImage is in use, convert it to Data URL
-      const imageForProcessing = userUploadedImage === imageToUse
-        ? await readAsDataURL(userUploadedImage)
-        : imageToUse;
+      // Debugging logs
+      console.log("Updated prediction status:", updatedPrediction.status);
   
-      const body = {
-        prompt: promptValue,
-        image: imageForProcessing,
-        mask: maskImage,
-      };
-  
-      const response = await axios.post("/api/predictions", body);
-      const newPrediction = response.data;
-  
-      setPredictions((prevPredictions) => [...prevPredictions, newPrediction]);
-  
-      let statusCheck = newPrediction.status;
-      while (statusCheck !== "succeeded" && statusCheck !== "failed") {
-        await sleep(1000);
-        const statusResponse = await axios.get(`/api/predictions/${newPrediction.id}`);
-        const updatedPrediction = statusResponse.data;
-        statusCheck = updatedPrediction.status;
-  
-        if (statusCheck === "succeeded" || statusCheck === "failed") {
-          setPredictions((prevPredictions) =>
-            prevPredictions.map(p => (p.id === updatedPrediction.id ? updatedPrediction : p))
-          );
-          if (statusCheck === "succeeded") {
-            // Clear the mask and set the uploaded image to the last successful output
-            setMaskImage(null);
-            setUserUploadedImage(updatedPrediction.output);
+      if (updatedPrediction.status === "succeeded") {
+        setPredictions(currentPredictions => {
+          // Ensure the last prediction is updated with the final output
+          const updatedPredictions = [...currentPredictions];
+          const indexToUpdate = updatedPredictions.findIndex(p => p.id === updatedPrediction.id);
+          if (indexToUpdate !== -1) {
+            updatedPredictions[indexToUpdate] = updatedPrediction;
           }
-        }
+          return updatedPredictions;
+        });
+  
+        setUserUploadedImage(null); // Clear the uploaded image since the prediction succeeded
+        break; // Exit the loop since the prediction has succeeded
+      } else if (updatedPrediction.status === "failed") {
+        setError("Prediction failed"); // Handle the failed prediction case
+        break; // Exit the loop since the prediction has failed
+      } else {
+        // Update predictions state to trigger re-render with new status
+        setPredictions(currentPredictions => {
+          const updatedPredictions = [...currentPredictions];
+          const indexToUpdate = updatedPredictions.findIndex(p => p.id === updatedPrediction.id);
+          if (indexToUpdate !== -1) {
+            updatedPredictions[indexToUpdate] = updatedPrediction;
+          }
+          return updatedPredictions;
+        });
       }
-    } catch (err) {
-      setError(err.response?.data.detail || "An error occurred");
     }
   };
   
-
-
 
   const startOver = () => {
     setPredictions([]);
@@ -98,7 +130,6 @@ export default function Home({ userData }) {
   };
 
   return (
-
     <div>
       <Head>
         <title>FullJourney.AI Inpainting</title>
@@ -113,20 +144,19 @@ export default function Home({ userData }) {
       <main className="container mx-auto p-2">
         {error && <div>{error}</div>}
 
-        {/* Brush size slider */}
         <div className="brush-slider-container text-white flex items-center justify-center mx-auto" style={{ width: '30%' }}>
-        <label htmlFor="brushSize" className="flex-shrink-0 mr-2">Brush Size: {brushSize}</label>
-        <input
-          type="range"
-          id="brushSize"
-          name="brushSize"
-          min="1"
-          max="100"
-          value={brushSize}
-          onChange={(e) => setBrushSize(Number(e.target.value))}
-          className="brush-slider flex-grow"
-        />
-      </div>
+          <label htmlFor="brushSize" className="flex-shrink-0 mr-2">Brush Size: {brushSize}</label>
+          <input
+            type="range"
+            id="brushSize"
+            name="brushSize"
+            min="1"
+            max="100"
+            value={brushSize}
+            onChange={(e) => setBrushSize(Number(e.target.value))}
+            className="brush-slider flex-grow"
+          />
+        </div>
 
         <div className="border-hairline max-w-[512px] mx-auto relative">
           <Dropzone
@@ -136,7 +166,6 @@ export default function Home({ userData }) {
           />
           <div
             className="bg-black relative max-h-[512px] w-full flex items-stretch  border-4 border-pink-400 rounded-xl"
-            // style={{ height: 0, paddingBottom: "100%" }}
           >
             <Canvas
               brushSize={brushSize}
@@ -165,20 +194,18 @@ export default function Home({ userData }) {
           </div>
         </div>
       </main>
-       {/* Add a logout button at the bottom of the page */}
-       <footer className="text-center my-4">
+      <footer className="text-center my-4">
         <button 
           onClick={handleLogout} 
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         >
-          Logout(env)
+          Logout
         </button>
       </footer>
     </div>
   );
 }
 
-// Helper function to read file as data URL
 function readAsDataURL(file) {
   return new Promise((resolve, reject) => {
     const fr = new FileReader();
@@ -190,17 +217,25 @@ function readAsDataURL(file) {
   });
 }
 
-// getServerSideProps remains unchanged, except removing the try-catch block for brevity
 export async function getServerSideProps(context) {
   const { req } = context;
   const cookies = req.headers.cookie || '';
 
-  const response = await axios.get('https://www.fulljourney.ai/api/auth/', {
-    headers: { Cookie: cookies },
-    withCredentials: true,
-  });
+  try {
+    const response = await axios.get('https://www.fulljourney.ai/api/auth/', {
+      headers: { Cookie: cookies },
+      withCredentials: true,
+    });
 
-  const userData = response.data;
-
-  return { props: { userData } };
+    const userData = response.data;
+    return { props: { userData } };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
 }
