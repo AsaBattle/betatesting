7d28c672409a3bdfa06e2c47f33355c83cc9af75
Asa a <asanbattle@gmail.com>
2023-12-28 16:06:40 -0800

    Added ENV var for replicate

--- pages/paint.js
+++ pages/paint.js
@@ -193,7 +193,7 @@ export default function Home( theUserData ) {
           onClick={handleLogout} 
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         >
-          Logout
+          Logout(Asa)
         </button>
       </footer>
     </div>
