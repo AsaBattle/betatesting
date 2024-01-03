074ccb17fa8009ed1fe362eeff0d4f345f8148d2
Asa a <asanbattle@gmail.com>
2024-01-02 16:14:16 -0800

    Fixed the flicker by preloading the image inside the dropzone component. Added WORKING_LOCALLY environment variable so login stuff is ignored when running on localhost

--- components/canvas.js
+++ components/canvas.js
@@ -1,88 +1,89 @@
-import React from "react";
-import Image from "next/image";
-import { ReactSketchCanvas } from "react-sketch-canvas";
-import Spinner from "components/spinner";
+import React, { useRef } from 'react';
+import Image from 'next/image';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+import Spinner from 'components/spinner';
 
-export default class Canvas extends React.Component {
-  constructor(props) {
-    super(props);
-    this.canvas = React.createRef();
-  }
+const Canvas = (props) => {
+  const canvasRef = useRef(null);
 
-  onChange = async () => {
-    const paths = await this.canvas.current.exportPaths();
+  // Handle the change event for the canvas
+  const onChange = async () => {
+    const paths = await canvasRef.current.exportPaths();
     if (paths.length) {
-      const data = await this.canvas.current.exportImage("svg");
-      this.props.onDraw(data);
+      const data = await canvasRef.current.exportImage('svg');
+      props.onDraw(data);
     }
   };
 
-  render() {
-    const predictions = this.props.predictions.map((prediction) => {
-      prediction.lastImage = prediction.output
-        ? prediction.output[prediction.output.length - 1]
-        : null;
-      return prediction;
-    });
+  // Process predictions to add lastImage property
+  const processedPredictions = props.predictions.map(prediction => ({
+    ...prediction,
+    lastImage: prediction.output ? prediction.output[prediction.output.length - 1] : null,
+  }));
 
-    const predicting = predictions.some((prediction) => !prediction.output);
-    const lastPrediction = predictions[predictions.length - 1];
+  const predicting = processedPredictions.some(prediction => !prediction.output);
+  const lastPrediction = processedPredictions[processedPredictions.length - 1];
 
-    return (
-         <div className="relative w-full aspect-square"
-        style={{
-          cursor: `url('/pen-cursor(w)2.png'), auto` // Ensure the cursor image file is in the public directory
-        }}
-      >
-        {/* PREDICTION IMAGES */}
-        {!this.props.userUploadedImage && predictions.filter((prediction) => prediction.output).map((prediction, index) => (
-          <Image
-            alt={"prediction" + index}
-            key={"prediction" + index}
-            layout="fill"
-            className="absolute animate-in fade-in"
-            style={{ zIndex: index }}
-            src={prediction.lastImage}
-          />
-        ))}
+  return (
+    <div
+      className="relative w-full aspect-square"
+      style={{
+        cursor: `url('/pen-cursor(w)2.png'), auto`
+      }}
+    >
+      {/* PREDICTION IMAGES */}
+      {!props.userUploadedImage && processedPredictions.filter(prediction => prediction.output).map((prediction, index) => (
+        <Image
+          alt={"prediction" + index}
+          key={"prediction" + index}
+          layout="fill"
+          className="absolute animate-in fade-in"
+          style={{ zIndex: index }}
+          src={prediction.lastImage}
+        />
+      ))}
 
-        {/* USER UPLOADED IMAGE */}
-        {this.props.userUploadedImage && (
-          <Image
-            src={URL.createObjectURL(this.props.userUploadedImage)}
-            alt="preview image"
-            layout="fill"
-          />
-        )}
+      {/* USER UPLOADED IMAGE */}
+      {props.userUploadedImage && (
+        <Image
+          src={props.userUploadedImage}  // Directly use the data URL
+          alt="preview image"
+          layout="fill"
+        />
+      )}
 
-        {/* SPINNER */}
-        {predicting && (
-          <div
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-            style={{ zIndex: predictions.length + 100 }}
-          >
-            <div className="p-4 w-40 bg-white text-center rounded-lg animate-in zoom-in">
-              <Spinner />
-              <p className="pt-3 opacity-30 text-center text-sm">
-                {lastPrediction.status}
-              </p>
-            </div>
+      {/* SPINNER */}
+      {predicting && (
+        <div
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          style={{ zIndex: processedPredictions.length + 100 }}
+        >
+          <div className="p-4 w-40 bg-white text-center rounded-lg animate-in zoom-in">
+            <Spinner />
+            <p className="pt-3 opacity-30 text-center text-sm">
+              {lastPrediction.status}
+            </p>
           </div>
-        )}
+        </div>
+      )}
 
-        {/* CANVAS FOR DRAWING */}
-        {(predictions.length > 0 || this.props.userUploadedImage) && !predicting && (
+      {/* CANVAS FOR DRAWING */}
+      {(processedPredictions.length > 0 || props.userUploadedImage) && !predicting && (
+        <div
+          className="absolute top-0 left-0 w-full h-full"
+          style={{ zIndex: processedPredictions.length + 100 }}
+        >
           <ReactSketchCanvas
-            ref={this.canvas}
-            strokeWidth={this.props.brushSize}
+            ref={canvasRef}
+            strokeWidth={props.brushSize}
             strokeColor="white"
             canvasColor="transparent"
-            onChange={this.onChange}
-            className="absolute top-0 left-0 w-full h-full"
-            style={{ zIndex: predictions.length + 100 }}
+            onChange={onChange}
           />
-        )}
-      </div>
-    );
-  }
-}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Canvas;
