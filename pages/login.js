0d3e52b1b17958967984b6081d2b1e5be08c5058
Asa a <asanbattle@gmail.com>
2024-02-07 13:03:04 -0800

    Newly made nextjsbeta redirect for login testing with beta only

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function Login() {
   useEffect(() => {
     // Redirect to the new Discord OAuth2 login page
-    window.location.href = 'https://www.fulljourney.ai/api/auth/nextjs';
+    window.location.href = 'https://www.fulljourney.ai/api/auth/nextjsbeta';
   }, []);
 
   return null;
