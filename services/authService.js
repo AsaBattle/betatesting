8a38bda9e07a5455aca0e79cfe3ad85d99b92583
Asa a <asanbattle@gmail.com>
2024-04-09 12:32:00 -0700

    Changed name of loginUser to check checkIfUserIsAlreadyLoggedIn, since that was what we were using that particular function for(The naming was previously wrong) Also, starting to get a consistent way to store the users login data(No mater which way they used to log in)

--- services/authService.js
+++ services/authService.js
@@ -1,12 +1,15 @@
-// services/authService.js
+  // we need to check if they have an account with us, if not, we create one
+  // and then we use need to modify our prediction api to make sure it's properly using the users account or at least make sure
+  // thats all still working
+  // Also need to create a template/default format for the user data so it's lways consistent no matter how they login
 
 import { getSession } from "next-auth/react";
 import axios from 'axios';
 import { serialize } from 'cookie';
 
 const AuthService = {
-  loginUser: async (req, res) => {
-    // Check if the user is logged in using FullJourney's authentication
+    checkIfUserIsAlreadyLoggedIn: async (req, res) => {
+    // Check if the user is logged in using FullJourney's(Discord) authentication
     const cookies = req.headers.cookie || '';
     try {
       const response = await axios.get('https://www.fulljourney.ai/api/auth/', {
@@ -14,7 +17,8 @@ const AuthService = {
         withCredentials: true,
       });
 
-      // If authentication is successful, serialize user data and set cookie
+      // If authentication was successful(meaning the user had already logged into their discord acocount via fj's login process), 
+      // Then we serialize the user data and create a cookie with it
       const userData = response.data;
       const serializedUserCookie = serialize('user', JSON.stringify(userData), {
         httpOnly: true,
@@ -26,13 +30,13 @@ const AuthService = {
 
       res.setHeader('Set-Cookie', serializedUserCookie);
       return userData;
-      
     } catch (error) {
-      console.error('Error with FullJourney authentication:', error);
-      // If FullJourney auth fails, we proceed to check for a NextAuth session
+      console.error('User was not logged in via Fulljourneys discord login process', error);
+      // If the user isn't already logged in via their discord proceed to check for a NextAuth session
     }
 
-    // Check if the user is logged in using NextAuth's session (Google in this case)
+    // Check if the user is logged through a NextAuth's session
+    // This could have been any of the providers we offer on our login screen(Right now it's just Google)
     const session = await getSession({ req });
     if (session) {
       // The user is logged in with NextAuth, we can return the session data
@@ -43,8 +47,6 @@ const AuthService = {
     // If no authentication method succeeded, return null
     return null;
   },
-
-  // ...you can add more helper functions for your authentication logic here
 };
 
 export default AuthService;
