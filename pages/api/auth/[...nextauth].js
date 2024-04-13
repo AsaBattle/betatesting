328bba34427165c96c2679c08864d443b765c7dd
Asa a <asanbattle@gmail.com>
2024-04-13 12:59:58 -0700

    we now search for a user by their email in our database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,6 +24,8 @@ export const authOptions = {
     },
     async session({ session, token }) {
 
+        console.log("session was called with session: ", session);
+
         // Retrieve the user ID from your database based on the user's email or other identifier
         const userId = await getUserIdFromDatabase("test");
 
@@ -41,9 +43,38 @@ export const authOptions = {
   // Other NextAuth configuration
 };
 
-// Returns the userID from the database based on the user's email
+// Returns the userID from the database based on the user's user name
 async function getUserIdFromDatabase(email) {
+    console.log("getUserIdFromDatabase was called with email: ", email);
+
+    let user = null;
+    if ((user=findUserByEmail(email)))
+        console.log("User was found in the database: ", user);
+    else
+        console.log("User was not found in the database");
+
+
   return 357; // Replace this with a real database query
+}
+
+
+async function findUserByEmail(email) {
+
+        // Try and fetch the user via their email from our database
+    let findUser;
+    try 
+    {
+        const response = await axios.get(`http://3.19.250.209:36734/user/${email}`);
+        findUser = response.data;
+        findUser.user_id = findUser.user_id.toString();
+    } catch (err) 
+    {
+        if (!err.response || err.response.status !== 404) {
+            throw err;
+        }
+    }
+
+    return findUser;
 }
 
 export default NextAuth(authOptions);
