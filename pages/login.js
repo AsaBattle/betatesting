5d527e63577cc47226071c85f259a47d5a4ff66b
Asa a <asanbattle@gmail.com>
2024-04-12 17:19:29 -0700

    fixed missing import

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import { useRef, useState, useEffect } from "react";
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useSession, signIn, signOut } from 'next-auth/react';
