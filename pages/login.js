ebd361081655592fd53a523b340aa13c0c9964bc
Asa a <asanbattle@gmail.com>
2024-04-30 16:06:24 -0700

    b

--- pages/login.js
+++ pages/login.js
@@ -94,7 +94,7 @@ const Login = () => {
     return (
         <div className={styles.container}>
             <div className={styles.header}>
-                <h3>aFullJourney Studio</h3>
+                <h3>bFullJourney Studio</h3>
                 <h3>Status is: {status}</h3> {/* Optional: Display authentication status */}
             </div>
             <div className={styles.text}>
