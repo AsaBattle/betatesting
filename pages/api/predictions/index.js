84cd3910eba4e24dcbcb298fac222ea8e9d84be4
Asa a <asanbattle@gmail.com>
2024-05-04 18:36:42 -0700

    finished adding code for free users so that they use their ipaddress to keep track of their credits

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -27,21 +27,36 @@ export default async function handler(req, res) {
     userData = JSON.parse(cookies.user || '{}');
 
     console.log("req.body.userId: ", req.body.userId);
-
-
+    
+    // If the user is logged in via ip(since they don't have an account yet)
+    
     // Now check to make sure the user has the necessary credits to make a prediction
-    details = await CheckAndSubtractCredits(userData, 1);
+    details = await CheckAndSubtractCredits(userData,req.body.ipUser, 1);
+
+    // Reasons for failure
+    // ip user doesn't have enough credits(Reasoncode 7)
+    // user doesn't have enough credits(Reasoncode 6)
+    // the user doesn't exist(Reasoncode 5)
+    // something else
+    
+
+    if (details.worked === false && details.reasonCode === 7) {
+      res.statusCode = 402;
+      console.log("Not enough credits left. Reasoncode 7");
+      res.end(JSON.stringify({ detail: details.reason, thecode: 5001 }));
+      return;
+    }
     if (details.worked === false && details.reasonCode === 6) {
       res.statusCode = 403;
       console.log("Not enough credits left. Reasoncode 6");
       res.end(JSON.stringify({ detail: details.reason, thecode: 5001 }));
       return;
     } else
-    if (details.worked === false &&details.reasonCode === 5) {
-      console.log("User doesn't exist. But this is ok, because we allow first time users some free images Reasoncode 5");
-      //res.statusCode = 404;
-      //res.end(JSON.stringify({ detail: details.reason, thecode: 5001 }));
-      //return;
+    if (details.worked === false && (details.reasonCode === 5 || details.reasonCode === 4)) {
+      console.log("User doesn't exist!!!");
+      res.statusCode = 404;
+      res.end(JSON.stringify({ detail: details.reason, thecode: 5001 }));
+      return;
     }
   } 
 
@@ -95,15 +110,48 @@ export default async function handler(req, res) {
 }
 
 
-
-async function CheckAndSubtractCredits(userData, creditsToSubtract) {
+// userID is the id used to log the user into...
+// if ipUser = true, then we're using the user's ip address to log them in and using that database
+// if ipUser = false, then we're using the user's id to log them in and using the main database
+async function CheckAndSubtractCredits(userID, ipUser, creditsToSubtract) {
   let response;
   let currentCredits;
   let newCredits;
 
+    // If the user is logged in via ip(since they don't have an account yet)
+  if (ipUser === true) {
+    // grab the user's current credits
+    try {
+        response = await axios.post('https://www.fulljourney.ai/api/auth/getFreeUserCredits', {
+        ipAddress: userID, 
+      });
+
+      currentCredits = response.data;
+      console.log('CheckAndSubtractCredits --- Ip address User credits:', response.data);
+    } catch (error) {
+      console.error('CheckAndSubtractCredits --- Error getting user credits:', error);
+      return {worked: false, reasonCode: 4, reason: "error retrieving ipuser credits"};
+    }
+    if (currentCredits <= 0)
+      return {worked: false, reasonCode: 7, reason: "IPUser - Not enough credits for image."};
+
+    try {
+      response = await axios.post(`http://www.fulljourney.ai/api/auth/modifyfreeusercredits`, {
+        ipAddress: userID,
+        credits: -creditsToSubtract,
+      });
+
+      return true;
+    } catch (error) {
+      console.error("Error when trying to update user credits with /modifyfreeusercredits." +error);
+      return  {worked: false, reason: "Error posting to server because of /modifyfreeusercredits."};
+    }
+  }
+
+
   // grab the user's current credits(userData has only the initial call to the server when the user logged in so it's not updated)
   try {
-    response = await axios.get(`http://3.19.250.209:36734/user/${userData.user_id}`);
+    response = await axios.get(`http://3.19.250.209:36734/user/${userID}`);
     const user = response.data;
     currentCredits = user.credits;
     currentCredits = parseInt(currentCredits);
@@ -122,13 +170,13 @@ async function CheckAndSubtractCredits(userData, creditsToSubtract) {
     return {worked: false, reasonCode: 6, reason: "Not enough credits for image."};
   }
 
-  console.log("UserData is: ", userData);
+  console.log("User's id is: ", userID);
   console.log("ok, currentCredits is:", currentCredits, "credits, subtracting:", creditsToSubtract, "credits, for a total of:", newCredits, "credits");
 
 
   // Now update the user's credits
   try {
-    const updateResult = await axios.post(`http://3.19.250.209:36734/user/${userData.user_id}`, {
+    const updateResult = await axios.post(`http://3.19.250.209:36734/user/${userID}`, {
       credits: newCredits,
     });
   } catch (error) {
