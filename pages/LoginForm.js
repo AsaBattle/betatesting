0913d5e3746e0333172f5ce2f3ac53b180c1493b
Asa a <asanbattle@gmail.com>
2024-05-08 16:57:27 -0700

    m

--- pages/LoginForm.js
+++ pages/LoginForm.js
@@ -122,7 +122,7 @@ const LoginForm = () => {
                         </button>
                         <button className={styles.discordBtn} onClick={handleFirebaseSignIn}>
                             <GiJourney className={styles.icon} />
-                            FJl
+                            FJm
                         </button>
                     </div>
                 </div>
