96837c09340a36776c1a8cd456a4f64257160a95
Asa a <asanbattle@gmail.com>
2024-05-14 17:47:44 -0700

    updated email as username

--- pages/LoginForm.js
+++ pages/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = () => {
                         console.log('...User email is verified!!!');
                         const result = await nextAuthSignIn('credentials', {
                             redirect: false,
-                            email: user.email,
+                            email: username,
                             password: password
                         });
                         if (result.error) {
