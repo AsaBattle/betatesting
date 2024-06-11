59d1f3162d9d06e7befccaf7bc67fb2820cfd39f
Asa a <asanbattle@gmail.com>
2024-06-10 17:59:40 -0700

    version update

--- pages/ImageMode.js
+++ pages/ImageMode.js
@@ -716,7 +716,7 @@ export default function Home(theUserData) {
           <meta name="viewport" content="initial-scale=0.7, width=device-width user-scalable=no" />
         </Head>
         <p className="pb-5 text-xl text-white text-center font-helvetica">
-          <strong>FullJourney.AI 0.1 Betak Studio</strong>
+          <strong>FullJourney.AI 0.1 Betaking Studio</strong>
         </p>
         <p className="text-white text-center font-helvetica">
           {userLoginNameAndCredits}
