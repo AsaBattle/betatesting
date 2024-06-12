4d36eefa536f83105f57626c191a70f975c52c45
Asa a <asanbattle@gmail.com>
2024-06-11 17:36:04 -0700

    the viewMode component, imported authService

--- pages/ViewMode.js
+++ pages/ViewMode.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import AuthService from '../services/authService';
 import styles from './ViewMode.module.css';
 
 export default function ViewMode( theUserData ) {
@@ -22,7 +23,7 @@ export default function ViewMode( theUserData ) {
 
   return (
     <div className={styles.viewMode}>
-      <h2>Your Generated Files</h2>
+      <h2>Yours Generated Files</h2>
       <div className={styles.fileGrid}>
         {files.map((file) => (
           <div key={file.name} className={styles.fileTile}>
