3724020837c3551710707549cf7042afcb3b46e7
Asa a <asanbattle@gmail.com>
2024-06-13 13:59:28 -0700

    bb

--- pages/ImageMode.js
+++ pages/ImageMode.js
@@ -772,7 +772,7 @@ export default function Home(theUserData) {
           <meta name="viewport" content="initial-scale=0.7, width=device-width user-scalable=no" />
         </Head>
         <p className="pb-5 text-xl text-white text-center font-helvetica">
-          <strong>FullJourney.AI 0.1 Beadering Studio</strong>
+          <strong>FullJourney.AI 0.1 BB Studio</strong>
         </p>
         <div className="flex flex-col items-center">
         <p className="text-white text-center font-helvetica">
