7a0960492faaca4b00283eaec24dce0310ada135
Asa a <asanbattle@gmail.com>
2024-06-17 18:04:09 -0700

    file path in fetchimage was wrong I guess

--- pages/api/fetchImage.js
+++ pages/api/fetchImage.js
@@ -19,22 +19,25 @@ export const config = {
     // Running locally
     storage = new Storage();
   }
-
-export default async function handler(req, res) {
-  const { imagePath } = req.query;
-
+  export default async function handler(req, res) {
+    const { imagePath } = req.query;
+  
     console.log('Inside fetchImage---- imagePath:', imagePath);
-
-  try {
-    const file = storage.bucket('fjusers').file(imagePath);
-    const [metadata] = await file.getMetadata();
-    const [contents] = await file.download();
-
-    res.setHeader('Content-Type', metadata.contentType);
-    res.send(contents);
-    console.log('I got the image!');
-  } catch (error) {
-    console.error('Error fetching image from GCS:', error);
-    res.status(500).json({ message: 'Failed to fetch image from GCS' });
+  
+    try {
+      // Extract the file path from the imagePath URL
+      const url = new URL(imagePath);
+      const filePath = url.pathname.slice(1); // Remove the leading '/'
+  
+      const file = storage.bucket('fjusers').file(filePath);
+      const [metadata] = await file.getMetadata();
+      const [contents] = await file.download();
+  
+      res.setHeader('Content-Type', metadata.contentType);
+      res.send(contents);
+      console.log('I got the image!');
+    } catch (error) {
+      console.error('Error fetching image from GCS:', error);
+      res.status(500).json({ message: 'Failed to fetch image from GCS' });
+    }
   }
-}
