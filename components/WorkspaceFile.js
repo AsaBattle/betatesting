6b84e85722f663436f2fe219ffff477b05e7dac3
Asa a <asanbattle@gmail.com>
2024-06-20 17:41:45 -0700

    Added capability to use FAL as a provider as well as Replicate.

---
+++ components/WorkspaceFile.js
@@ -0,0 +1,7 @@
+/*
+This component is responsible for saving and loading the workspace, using the user GCS bucket as a receptacle for the workspace files.
+The user's workspace is saved in the GCS bucket with the user's email as their folder name and fjuserworkpspace.dat as the file name.
+The workspace is saved as a JSON object containing the following fields:
+- Current image save path
+- List of gcs bucket files currently being worked on
+*/
